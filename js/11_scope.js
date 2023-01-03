6fde8315f6fb1e82ec1a2d10230f991e59488ab0
azharcool <azharuddinmullah219@gmail.com>
2023-01-03 15:49:17 +0530

    added scope and array

--- js/11_scope.js
+++ js/11_scope.js
@@ -1,39 +1,90 @@
-// Scope types
-// 1. global scope
-// 2. function scope
-// 3. block scope
+/* 
+  Scope
+  ------
+  1. scope is visibility or access of variable or function.
+  2. Scope types:  
+    1. global scope -> it should be outside of function or block
+    2. lobal scope (block scope or function scope) -> it should be inside of function or block.
 
-// var x = 10; // declared
-// var x = 20; // re-declared
-// x = 30; // re-initialize
+  difference between var, let, and const
+  1. var ->
+    1. It is function scope, if variable declared inside function otherwise global scope.
+    2. It can be re-declared.
+    3. It can be re-initialized.
 
-// let y = 10;
-// let y = 20; // can't re-declared
-// y = 30// can  re-initialize
+  2. let -> 
+    1. It is block scope, if variable declared inside curly block otherwise global scope.
+    2. It can't be re-declared.
+    3. It can be re-initialized.
 
-// const z = 10;
-// const z = 20; // can't re-declared
-// z= 30; //can't initialize
+  3. const -> 
+    1.  It is block scope, if variable declared inside curly block otherwise global scope.
+    2. It can't be re-declared.
+    3. It can't be re-initialized.
 
-// Global Scope
-var x = 1; // function scope
-let y = 2; // block scope
-const z = 3; // block scope
+*/
 
-// Local or block Scope
+// Variable declared with initialization
+var myVar = "helo";
+
+// Variable re-declared
+var myVar = true;
+
+// Variable re-initialize
+var myVar = "helo"; // already value assigned
+myVar = true; // re-assigned  (re-initialized)
+myVar = 0; // re-assigned  (re-initialized)
+myVar = 20; // re-assigned  (re-initialized)
+
+var myNumber = 10;
+// Regular function  -> function delcaration
+function myfunction() {
+  var myFunNumber = 30;
+  console.log(`my functio block ${myFunNumber}`);
+}
+
+var x = 10; // global scope
+let y = 20; // global scope
+const z = 30; // global scope
+
 {
-  let y = 4;
-  console.log(y);
+  let y = 30; // local scope or block
+  const z = 40; // local scope or block
+  var x = 30; //global scope
+
+  console.log(`block scope of y: ${y}`);
+  console.log(`block scope of z: ${z}`);
 }
 
-// function Scope
-function myFunc() {
-  let z = 5;
-  console.log(z);
+console.log(`global scope of x:  ${x}`); // 30
+console.log(`global scope of y:  ${y}`); // 20
+console.log(`global scope of z:  ${z}`); // 30
 
-  // Local or block Scope
+function myFunction() {
+  var x = "fu";
+  let y = "Helo";
+  const z = "no-value";
+
   {
-    let y = 4;
-    console.log(y);
+    let y = 30; // local scope or block
+    const z = 40; // local scope or block
+    var x = 30; //global scope
+
+    console.log(`block scope of y: ${y}`);
+    console.log(`block scope of z: ${z}`);
   }
+
+  if(true){
+    var x = "redeclared"
+
+    {
+      let y = 30; 
+    }
+  }
+
+  console.log(`function scope of x: ${x}`); //30
+  console.log(`block scope of y: ${y}`);
+  console.log(`block scope of z: ${z}`);
 }
+
+myFunction();
--- js/12_array.js
+++ js/12_array.js
@@ -1,55 +1,111 @@
 // Arrays
+// 1. Array properties. (length)
+// 2. Add element in an array.
+// 3. How to access array element.
+// 4. How to access last element of array.
+// 5. Array methods (push, pop, shift, unshift, reverse).
+// 6. Remove element from Array using splice.
+// 7. Replace element from Array using splice.
+// 8. Add element at specific index using splice.
+// 10. Convert array to string.
+// 11. Combine array
+
+/* 
+Arrays -> collection of different data type of element.
+1. How to create array.
+2. Add element in an array.
+3. How to access element from an array.
+4. How to access last element from an array.
+5. Array Methods (push, pop, unshift, shift, splice, reverse, map)
+6. delete keyword.
+7. splice (Add, remove, replace at pos)
+8. convert array to string.
+9. convert string to array.
+10. combine array
+11. ES6 feature -> spread operator, rest operator
+
+Spread Operator -> Extract value from array or object.
+Rest Operator -> Transform value into array.
+
+*/
+
+// Array create
 var myArray = [];
 
-// add element in array
-myArray[0] = "helo";
+// add element in an array
+myArray[0] = 10;
 myArray[1] = true;
-myArray[2] = 32;
+myArray[2] = "azhar";
 
 console.log(myArray);
 
-// Array properties
+// Access array element
+console.log(myArray[2]);
+
+// array properties
 console.log(myArray.length);
 
-// last element of array
+// access last element of an array
 console.log(myArray[myArray.length - 1]);
 
-// LILO
-myArray.push("school");
-myArray.pop();
+// Array Methods (push, pop, unshift, shift, splice)
+// splice -> can remove, add, replace at particular position from an array.
+const stack = ["js", "reactJS"];
+// stack.push("react-native");
+// const newLength = stack.push("angular");
+// const popped = stack.pop();
 
-// FIFO
-myArray.unshift(42);
-myArray.shift();
+stack.unshift("css");
+stack.unshift("html");
 
-delete myArray[1]; //don't use
+// stack.shift();
 
-// remove
-myArray.splice(1, 1);
+// console.log(stack);
 
-// replace
-myArray.splice(1, 1, 42);
+// Remove element from an array
+// delete stack[2]; // don't use
+// stack.splice(1, 1);
+// console.log(stack);
 
-// add element specific index
-myArray.splice(1, 0, 42);
+// stack.splice(1, 0, "html5", "css3", "scss");
+// stack.splice(1, 1, "css3")
 
-// reverse array
-myArray.reverse();
+// stack.reverse()
 
-// convert to string
-const arrToStr = myArray.join();
+// Converted array to string
+const stackStr = stack.join(", ");
 
-// convert to array
-const strToArr = myArray.split(",");
+// Convert string to array
+const stackArr = stackStr.split(",");
 
-// combine array
-const arr1 = [1,2];
-const arr2 = [3,4];
-const newArr = arr1.concat(arr2);
-const newArr1 = [...arr1,...arr2];
-const newArr2 = [arr1,arr2];
+// concat
+const arr1 = ["html", "css"];
+const arr2 = ["js", "reactjs"];
 
+// concat()
+const combineArr = arr2.concat(arr1);
 
+// ES6 feature -> using spread operator
+const newArr = [...arr1, ...arr2]; // ["html", "css", ["js", "react"]]
 
+// ES6 feature -> using destructure
+const [html] = newArr;
 
-console.log(myArray);
+// ES6 feature -> Rest operator
+const [html1, css, ...rest] = newArr;
+
+console.log(html, newArr[0], rest);
+
+console.log(newArr);
+
+const myNumArr = [2, 3, 4, 5]; // [4, 5, 6, 7]
+
+const newMyNumArr = [];
+for (let index = 0; index < myNumArr.length; index++) {
+  newMyNumArr.push(myNumArr[index] + 2);
+}
+
+const added = myNumArr.map((item) => item + 2);
+
+console.log(myNumArr);
+console.log(added);
